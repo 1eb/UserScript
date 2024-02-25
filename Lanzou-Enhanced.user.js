5be888a99276c43033751574393bc4bf921455eb
bin <97284148+b1n23@users.noreply.github.com>
2024-02-25 11:59:10 +0800

    Update Lanzou-Enhanced.user.js

--- Lanzou-Enhanced.user.js
+++ Lanzou-Enhanced.user.js
@@ -1,15 +1,16 @@
 // ==UserScript==
 // @name         蓝奏云盘增强
-// @namespace    https://github.com/1eb/UserScript
+// @namespace    https://github.com/b1n23/UserScript/blob/main/Lanzou-Enhanced.user.js
 // @version      0.1
 // @description  蓝奏云盘个人中心隐藏充值遮罩
-// @author       1eb
+// @author       b1n23
 // @match        *://pc.woozooo.com/mydisk.php?item=profile&action=mypower
 // @match        *://www.lanzou.com/mydisk.php?item=profile&action=mypower
 // @icon         https://www.lanzou.com/favicon.ico
 // @grant        none
-// @license      GPLv3
-// @homepageURL  https://github.com/1eb/UserScript
+// @homepageURL  https://github.com/b1n23/UserScript
+// @updateURL    https://github.com/b1n23/UserScript/raw/main/Lanzou-Enhanced.user.js
+// @downloadURL  https://github.com/b1n23/UserScript/raw/main/Lanzou-Enhanced.user.js
 // ==/UserScript==
 
 (function() {
